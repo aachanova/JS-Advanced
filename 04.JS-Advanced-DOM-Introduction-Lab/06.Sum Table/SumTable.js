977589275ab1b31aa77bd6bfb84b99a82185bef1
Aneliya Achanova <anelia_9@yahoo.com>
2024-09-04 23:45:23 +0300

    created 06.SumTable

--- 04.JS-Advanced-DOM-Introduction-Lab/06.Sum Table/SumTable.js
+++ 04.JS-Advanced-DOM-Introduction-Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
+    let prices = document.querySelectorAll('table td:nth-child(2):not(#sum)');
 
+    let sum = 0;
+
+    for (let price of prices) {
+        sum += Number(price.textContent);
+    }
+
+    document.getElementById('sum').textContent = sum;
 }
