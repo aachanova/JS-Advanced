3f5969252bdffa94281a72a40a3b98aaded228a5
Aneliya Achanova <anelia_9@yahoo.com>
2024-09-04 19:36:52 +0300

    created 03.Sum Numbers

--- 04.JS-Advanced-DOM-Introduction-Lab/03.Sum Numbers/SumNumbers.js
+++ 04.JS-Advanced-DOM-Introduction-Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,7 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const num1Element = document.getElementById('num1');
+    const num2Element = document.getElementById('num2');
+    const sumElement = document.getElementById('sum');
+
+    sumElement.value = Number(num1Element.value) + Number(num2Element.value);
 }
