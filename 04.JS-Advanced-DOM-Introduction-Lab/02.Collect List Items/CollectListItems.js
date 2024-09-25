fe5247fc2a31eb30fd937b0703ae653f9e8ae055
Ani <anelia_9@yahoo.com>
2024-09-25 23:24:34 +0300

    changes in extractText

--- 04.JS-Advanced-DOM-Introduction-Lab/02.Collect List Items/CollectListItems.js
+++ 04.JS-Advanced-DOM-Introduction-Lab/02.Collect List Items/CollectListItems.js
@@ -2,6 +2,8 @@ function extractText() {
     let itemsElements = document.querySelectorAll('#items li');
     let textAreaElement = document.getElementById('result');
 
+    textAreaElement.value = '';
+    
     for (const item of itemsElements) {
         textAreaElement.value += item.textContent + '\n';
     }
