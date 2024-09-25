ff89fc60a14bebbb5a3000ff71346b69ab5f7c47
Aneliya Achanova <148455328+aachanova@users.noreply.github.com>
2024-09-25 21:55:25 +0300

    Delete 04.JS-Advanced-DOM-Introduction-Lab2/01.Edit Element directory

--- 04.JS-Advanced-DOM-Introduction-Lab2/01.Edit Element/01.EditElement2.js
+++
@@ -1,8 +0,0 @@
-function editElement() {
-    const regExp = new RegExp('%insert name here%', 'g');
-    const replacer = 'Document Object Model';
-
-    document.querySelectorAll('h1').forEach(e => {
-        e.textContent = e.textContent.replace(regExp, replacer);
-    });
-}
