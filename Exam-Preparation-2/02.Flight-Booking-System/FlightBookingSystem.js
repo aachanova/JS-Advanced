3c4e3c47a78b1e1fa30f059c1b6e9989b0e9c188
Aneliya Achanova <anelia_9@yahoo.com>
2024-10-14 14:49:23 +0300

    changes on FlightBookingSystem.js

--- Exam-Preparation-2/02.Flight-Booking-System/FlightBookingSystem.js
+++ Exam-Preparation-2/02.Flight-Booking-System/FlightBookingSystem.js
@@ -1,8 +1,8 @@
 class FlightBookingSystem {
     constructor(agencyName) {
         this.agencyName = agencyName;
-        this.flights = [];
-        this.bookings = [];
+        this.flights = [];  // { flightNumber, destination, departureTime, price }
+        this.bookings = []; // { passengerName, flightNumber }
         this.bookingsCount = 0;
     }
 
@@ -46,11 +46,52 @@ class FlightBookingSystem {
     };
 
     showBookings(criteria) {
-        const criterias = ['all', 'cheap', 'expensive'];
+        if (this.bookings.length == 0) {
+            throw new Error('No bookings have been made yet.');
 
-        
-    };
-}
+        }
+        if (criteria === 'all') {
+            let result = [`All bookings(${this.bookingsCount}):`];
+            for (const booking of this.bookings) {
+                result.push(`${booking.passengerName} booked for flight ${booking.flightNumber}.`)
+            }
+
+            return result.join('\n');
+        } else if (criteria === 'cheap') {
+            let cheapFlights = this.flights.filter(f => f.price <= 100);
+            if (cheapFlights.length == 0) {
+                return `No cheap bookings found.`
+            } else {
+                let result = [`Cheap bookings:`];
+                for (const flight of cheapFlights) {
+                    let currentFlightNumber = flight.flightNumber;
+                    let currentBooking = this.bookings.find(b => b.flightNumber == currentFlightNumber);
+                    if (currentBooking) {
+                        result.push(`${currentBooking.passengerName} booked for flight ${currentFlightNumber}`);
+                        return result.join('\n');
+                    }
+                }
+            }
+        } else if (criteria == 'expensive') {
+            let expensiveFlights = this.flights.filter(f => f.price > 100);
+            if (expensiveFlights.length == 0) {
+                return `No expensive bookings found.`
+            } else {
+                let result = [`Expensive bookings:`];
+                for (const flight of expensiveFlights) {
+                    let currentFlightNumber = flight.flightNumber;
+                    let currentBooking = this.bookings.find(b => b.flightNumber == currentFlightNumber);
+                    if (currentBooking) {
+                        result.push(`${currentBooking.passengerName} booked for flight ${currentFlightNumber}`);
+                        return result.join('\n');
+                    }
+                }
+            }
+        }
+    }
+};
+
+
 
 // const system = new FlightBookingSystem("TravelWorld");
 // console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
@@ -65,12 +106,12 @@ class FlightBookingSystem {
 // console.log(system.bookFlight("Bob", "BB202"));
 // console.log(system.bookFlight("Charlie", "CC303"));
 
-const system = new FlightBookingSystem("TravelWorld");
-console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
-console.log(system.addFlight("BB202", "New York", "10:30 AM", 180));
-console.log(system.bookFlight("Alice", "AA101"));
-console.log(system.bookFlight("Bob", "BB202"));
-console.log(system.cancelBooking("Alice", "AA101"));
+// const system = new FlightBookingSystem("TravelWorld");
+// console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
+// console.log(system.addFlight("BB202", "New York", "10:30 AM", 180));
+// console.log(system.bookFlight("Alice", "AA101"));
+// console.log(system.bookFlight("Bob", "BB202"));
+// console.log(system.cancelBooking("Alice", "AA101"));
 
 // const system = new FlightBookingSystem("TravelWorld");
 // console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
@@ -79,11 +120,10 @@ console.log(system.cancelBooking("Alice", "AA101"));
 // console.log(system.bookFlight("Bob", "BB202"));
 // console.log(system.showBookings("all"));
 
-// const system = new FlightBookingSystem("TravelWorld");
-
-// console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
-// console.log(system.addFlight("BB202", "New York", "10:30 AM", 180));
-// console.log(system.bookFlight("Alice", "AA101"));
-// console.log(system.bookFlight("Bob", "BB202"));
-// console.log(system.showBookings("expensive"));
-// console.log(system.showBookings("cheap")); 
+const system = new FlightBookingSystem("TravelWorld");
+console.log(system.addFlight("AA101", "Los Angeles", "09:00 AM", 250));
+console.log(system.addFlight("BB202", "New York", "10:30 AM", 180));
+console.log(system.bookFlight("Alice", "AA101"));
+console.log(system.bookFlight("Bob", "BB202"));
+console.log(system.showBookings("expensive"));
+console.log(system.showBookings("cheap")); 
