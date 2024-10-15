1beedd339922bac2959d3cb6c3a2a5abc22d9e7f
Aneliya Achanova <anelia_9@yahoo.com>
2024-10-15 12:02:58 +0300

    made changes

--- Exam-Preparation-2/01.Tech-Troubleshoot-Hub-Resources/app.js
+++ Exam-Preparation-2/01.Tech-Troubleshoot-Hub-Resources/app.js
@@ -1,9 +1,35 @@
 window.addEventListener('load', solution);
 
 function solution() {
-  //TODO...
+  const employeeInputElement = document.getElementById('employee');
+  const categoryInputElement = document.getElementById('category');
+  const urgencyInputElement = document.getElementById('urgency');
+  const teamInputElement = document.getElementById('team');
+  const descriptionInputElement = document.getElementById('description');
+
+  const addButtonElement = document.getElementById('add-btn');
+  addButtonElement.addEventListener('click', onAdd);
+
+  const ulPreviewSection = document.getElementById('preview-list');
+  const ulPendingSection = document.getElementById('pending-list');
+  const ulResolvedSection = document.getElementById('resolved-list');
+
+  const employeeInput = employeeInputElement.value;
+  const categoryInput = categoryInputElement.value;
+  const urgencyInput = urgencyInputElement.value;
+  const teamInput = teamInputElement.value;
+  const descriptionInput = descriptionInputElement.value;
+
+  function onAdd(e) {
+    e.preventDefault();
+
+    if (!employeeInput || !categoryInput || !urgencyInput || !teamInput || !descriptionInput) {
+      return;
+    }
+
+  }
 }
 
 
-    
-    
+
+
